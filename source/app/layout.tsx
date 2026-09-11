dfef121e7d126ae9498ee6d844273def2ac1881b
EvanR12345 <101021488+EvanR12345@users.noreply.github.com>
2026-09-11 12:17:56 -0400

    Add Google AdSense loader site-wide

--- source/app/layout.tsx
+++ source/app/layout.tsx
@@ -18,6 +18,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4610762209559364"
+          crossOrigin="anonymous"
+        />
+      </head>
       <body>{children}</body>
     </html>
   );
